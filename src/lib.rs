5794f32deb2642c5174b49d95624e5483de12408
person93 <person93.person93@gmail.com>
2022-03-28 21:34:04 -0400

    feat: add "tasks" macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
 #![forbid(unsafe_code)]
 
 pub mod prelude {}
+
+#[macro_export]
+macro_rules! tasks {
+  ($($name:ident)*) => {
+    $(mod $name;)*
+
+    #[derive(Subcommand)]
+    #[allow(non_camel_case_types)]
+    enum Task {
+      $($name($name::Cli)),*
+    }
+
+    impl Task {
+      fn run(self) -> ! {
+        match self {
+          $(Task::$name(cli) => self::$name::main(cli)),*
+        }
+      }
+    }
+  };
+}
