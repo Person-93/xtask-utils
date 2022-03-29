50777784c31da5e1b2b7d7d324b47939c7dc1b26
person93 <person93.person93@gmail.com>
2022-03-29 09:26:08 -0400

    test: add test for pipe

---
+++ tests/pipe.rs
@@ -0,0 +1,69 @@
+use std::{
+  env, fs,
+  time::{SystemTime, UNIX_EPOCH},
+};
+use xtask_utils::pipe;
+
+#[test]
+fn happy_path() {
+  let mut temp = env::temp_dir().join(env!("CARGO_PKG_NAME")).join(
+    SystemTime::now()
+      .duration_since(UNIX_EPOCH)
+      .unwrap()
+      .as_micros()
+      .to_string(),
+  );
+  temp.set_extension("txt");
+
+  pipe!("echo hello world" | "cat" | "cat" > temp.clone())
+    .wait()
+    .unwrap();
+
+  assert_eq!(
+    fs::read_to_string(&temp)
+      .unwrap_or_else(|_| panic!(
+        "failed to read temp file: {}",
+        temp.display()
+      ))
+      .trim(),
+    "hello world"
+  )
+}
+
+#[test]
+fn single_command_fail() {
+  let result = pipe!(FAIL_CMD).spawn().unwrap().join().unwrap();
+  assert!(!result.success());
+}
+
+#[test]
+fn multi_command_first_fail() {
+  let result = pipe!(FAIL_CMD | "cat" | "cat")
+    .spawn()
+    .unwrap()
+    .join()
+    .unwrap();
+  assert!(!result.success());
+}
+
+#[test]
+fn multi_command_last_fail() {
+  let result = pipe!("echo hello" | "cat" | FAIL_CMD)
+    .spawn()
+    .unwrap()
+    .join()
+    .unwrap();
+  assert!(!result.success());
+}
+
+#[test]
+fn multi_command_mid_fail() {
+  let result = pipe!("echo hello" | FAIL_CMD | "echo")
+    .spawn()
+    .unwrap()
+    .join()
+    .unwrap();
+  assert!(!result.success());
+}
+
+const FAIL_CMD: &str = "cargo run --manifest-path test-helper -- exit-code 1";
